34b424cd0046d780cd9ce4ee9444bb603c3107fa
Paul Cartwright <pauljamescartwright@gmail.com>
2016-01-31 00:58:23 -0600

    Worked on knockknock server

--- knockkncokclient.py
+++ knockkncokclient.py
@@ -3,42 +3,34 @@ import os
 
 from socket import *
 
-def getMyIp():
-    s = socket(AF_INET, SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return (s.getsockname()[0])
+def getServerIp():
+    responce = input("Enter the servers IP:")
+    if responce.strip() == "":
+        s = socket(AF_INET, SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        SimpleClient(s.getsockname()[0])
+    else:
+        SimpleClient(responce.strip())
 
-def SimpleClient():
+def SimpleClient(IP):
     os.system("clear")
     keepgoing = True
     #while keepgoing == True:
     client = socket(AF_INET, SOCK_STREAM)
-    client.connect((getMyIp(),2000))
+    client.connect((IP,2000))
     msg = client.recv(1024)
     print(msg.decode("ascii"))
     responce = "\tWho's there?"
     print(responce)
     client.send(responce.encode("ascii"))
-    msg = client.recv(1024)
-    print(msg.decode("ascii"))
+    data = client.recv(1024)
+    msg = data.decode("ascii")
+    print(msg)
     responce = str("\t" + str(msg) + " who?")
     print(responce)
     client.send(responce.encode("ascii"))
+    data = client.recv(1024)
+    msg = data.decode("ascii")
+    print(msg)
 
-"""
-        print("\n*1...dvfdv.\n*2...Help?\n*0...Quit!")
-        usr_input = int(input("Option: *"))
-        if usr_input == 1:
-            print("Enter Command: ")
-            da_input = str(input())
-            client.send(da_input.encode("ascii"))
-        elif usr_input == 2:
-            client.send("help".encode("ascii"))
-        elif usr_input == 0:
-            client.send("bye!".encode("ascii"))
-            keepgoing = False
-        os.system("clear")
-        msg = client.recv(1024)
-        print(msg.decode("ascii"))
-"""
-SimpleClient()
+getServerIp()
--- knockknock.py
+++ knockknock.py
@@ -18,57 +18,34 @@ def KnockKnock():
     serversocket.listen(1)
     print("...Listening...")
     dont_stop = True
-    #while dont_stop == True:
-    clientsocket,addr = serversocket.accept()
-    print("connection from : ", addr[0])
-    responce = "Knock-Knock"
-    print(responce)
-    clientsocket.send(responce.encode("ascii"))
-    data = clientsocket.recv(1024)
-    msg = data.decode("ascii")
-    print(msg)
-    if msg == "\tWho's there?":
-        jokes = open("jokes.txt")
-        lines = []
-        for row in jokes:
-            items = row.strip().split(",")
-            lines.append(tuple(items))
-        rand = random.randint(0, len(lines))
-        responce = lines[rand][0]
+    while dont_stop == True:
+        print("Waiting...")
+        clientsocket,addr = serversocket.accept()
+        print("connection from : ", addr[0])
+        responce = "Knock-Knock"
         print(responce)
         clientsocket.send(responce.encode("ascii"))
-    data = clientsocket.recv(1024)
-    msg = data.decode("ascii")
-    print(msg)
-
-
-    """
         data = clientsocket.recv(1024)
         msg = data.decode("ascii")
-        if msg == "whoareyou?":
-            clientsocket.send(gethostname().encode("ascii"))
-        elif msg == "whenisit?":
-            clientsocket.send(str(time.asctime( time.localtime(time.time()) )).encode("ascii"))
-        elif msg == "whatdidyousay?":
-            words = open("words")
-            list = []
-            for line in words:
-                list.append(line.strip())
-            num = random.randint(0, len(list))
-            word = list[num]
-            clientsocket.send(str(word).encode("ascii"))
-        elif msg[:4] == "add,":
-            msg_list = msg.split(",")
-            sum = int(msg_list[1]) + int(msg_list[2])
-            clientsocket.send(str(sum).encode("ascii"))
-        elif msg == "bye!":
-            dont_stop = False
-            clientsocket.send("See you later!".encode("ascii"))
-        elif msg == "help":
-            clientsocket.send("Who I am: \"whoareyou?\"\nWhen it is: \"whenisit?\"\nWhat I said: \"whatdidyousay?\"\nAdding numbers x and y: \"add,x,y\"\nWhen you want to hangup: \"bye!\"".encode("ascii"))
-        else:
-            clientsocket.send(str("Huh? : " + msg + "\nWho I am: \"whoareyou?\"\nWhen it is: \"whenisit?\"\nWhat I said: \"whatdidyousay?\"\nAdding numbers x and y: \"add,x,y\"\nWhen you want to hangup: \"bye!\"").encode("ascii"))
-    """
+        print(msg)
+        if msg == "\tWho's there?":
+            jokes = open("jokes.txt")
+            lines = []
+            for row in jokes:
+                items = row.strip().split(",")
+                lines.append(tuple(items))
+            rand = random.randint(0, len(lines)-1)
+            responce = lines[rand][0]
+            print(responce)
+            clientsocket.send(responce.encode("ascii"))
+        data = clientsocket.recv(1024)
+        msg = data.decode("ascii")
+        print(msg)
+        responce = lines[rand][1]
+        responce = responce.strip()
+        print(responce)
+        clientsocket.send(responce.encode("ascii"))
+
     clientsocket.close()
     #serversocket.close()
 
